7d12665a061d3a5cc74eb6b3ab271d0b42c42b72
bogaz <bagusnurahmad7@gmail.com>
2023-09-12 07:30:57 +0700

    register modal form

--- interactive.js
+++ interactive.js
@@ -31,3 +31,61 @@ function updateIconVisibility() {
         showPassword.style.display = "none";
     }
 }
+
+// Interaktive invalid register input
+const firstName = document.getElementById("firstName");
+const endName = document.getElementById("endName");
+const emailInput = document.getElementById("emailInput");
+const password = document.getElementById("password");
+
+// Firstname
+firstName.addEventListener("focus", (event) => {
+    firstName.classList.remove("is-invalid")
+})
+
+firstName.addEventListener("blur", (event) => {
+    if (firstName.value === "") {
+        firstName.classList.add("is-invalid")
+    } else {
+        firstName.classList.remove("is-invalid")
+    }
+})
+
+// Endname
+endName.addEventListener("focus", (event) => {
+    endName.classList.remove("is-invalid")
+})
+
+endName.addEventListener("blur", (event) => {
+    if (endName.value === "") {
+        endName.classList.add("is-invalid")
+    } else {
+        endName.classList.remove("is-invalid")
+    }
+})
+
+// Email
+emailInput.addEventListener("focus", (event) => {
+    emailInput.classList.remove("is-invalid")
+})
+
+emailInput.addEventListener("blur", (event) => {
+    if (emailInput.value === "") {
+        emailInput.classList.add("is-invalid")
+    } else {
+        emailInput.classList.remove("is-invalid")
+    }
+})
+
+// Password
+password.addEventListener("focus", (event) => {
+    password.classList.remove("is-invalid")
+})
+
+password.addEventListener("blur", (event) => {
+    if (password.value === "") {
+        password.classList.add("is-invalid")
+    } else {
+        password.classList.remove("is-invalid")
+    }
+})
